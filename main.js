6862215be31bcb0de590fb4468fa40ba8adf217c
sharon smith <sharon.e.smith2@gmail.com>
2017-03-06 19:23:17 -0600

    navbar in index.html

--- main.js
+++ main.js
@@ -1,2 +1,60 @@
 
 console.log("writing to console");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SHE DELETED ALL OF THIS:
+// XHR :: XMLHttpRequest
+// Allows you to transfer data from one place to another
+
+// two formats:
+// JSON -- most common ; Javascript Object Notation / can use interchangeably w any C-based language
+// XML -- sadness
+
+// var x = 5;
+
+// div.innerHTML = x; // synchronist request
+
+// var y = 4;
+
+// div.innerHTML += y // =9
+
+
+
+
+// var z = some XHR request;
+
+// div.innerHTML += z; /// javascript won't wait for the XHR request :: undefined
+
+// function doStuff(){
+// 	div.innerHTML += z // javascript is ok if statement is wrapped in a function
+// }
+// div.addEventListener("click", doStuff)
