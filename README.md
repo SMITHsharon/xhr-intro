636131a57b5d940c70985cf18df02de5b3c1a741
sharon smith <sharon.e.smith2@gmail.com>
2017-03-06 21:19:31 -0600

    finished with class exercise

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # xhr-intro
+# classwork
--- main.js
+++ main.js
@@ -1,8 +1,34 @@
 
+console.log("First line in JS file: ", Date.now());
+
 // console.log("writing to console");
+var dinoContainer = document.getElementById("dinoContainer");
+function makeDOM(xhrData){
+
+	var dinosaurString = "";
+	var currentDinosaur;
+
+	for (var i=0; i<xhrData.dinosaurs.length; i++) {
+		currentDinosaur = xhrData.dinosaurs[i];
+		
+			dinosaurString += `<div class="col-sm-6 col-md-4">`;
+			dinosaurString += `<div class="thumbnail">`;
+			dinosaurString += `<img src="${currentDinosaur.url}" alt="dino">`;
+			dinosaurString += `<div class="caption">`;
+			dinosaurString += `<h3>${currentDinosaur.name}</h3>`;
+			dinosaurString += `<p>Is a ${currentDinosaur.type}</p>`;
+			dinosaurString += `<p>Like to eat: ${currentDinosaur.food}</p>`;     
+			dinosaurString += `</div></div></div>`;
+	}
+	dinoContainer.innerHTML = dinosaurString;
+}
 
 function executeThisCodeAfterFileLoaded(){
-	console.log("It worked!!!");
+	// console.log("It worked!!!", this.responseText);
+	console.log("Data Returned: ", Date.now());
+	var data = JSON.parse(this.responseText);
+	// console.log("It worked!!!", data);
+	makeDOM(data);
 }
 
 function executeThisCodeAfterFileFails(){
@@ -21,7 +47,9 @@ myRequest.addEventListener("load", executeThisCodeAfterFileLoaded);
 myRequest.addEventListener("error", executeThisCodeAfterFileFails);
 myRequest.open("GET", "dinosaurs.json");
 myRequest.send();
-console.log("myRequest", myRequest);
+// console.log("myRequest", myRequest);
+
+console.log("Last line in JS file: ", Date.now());
 
 // GET
 // POST :: sending file
